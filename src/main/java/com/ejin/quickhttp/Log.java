4a6eddb22b876604f411b13e67c998b898891f74
ejin <sulei.jin@lifesense.com>
2018-03-29 22:14:30 +0800

    update

--- src/main/java/com/ejin/quickhttp/Log.java
+++ src/main/java/com/ejin/quickhttp/Log.java
@@ -11,23 +11,16 @@ class Log {
     private static final int INFO = 6;
 
     static void d(String msg) {
-//        System.out.println(pack(msg));
-        Platform.get().log(INFO, pack(msg), null);
-    }
-
-    static void i(String msg) {
-//        System.out.println(pack(msg));
         Platform.get().log(INFO, pack(msg), null);
     }
 
     static void e(String msg) {
-//        System.err.println(pack(msg));
         Platform.get().log(WARN, pack(msg), null);
     }
 
     private static String pack(String msg) {
         if (msg == null || "".equals(msg)) {
-            msg = "print empty message";
+            msg = "null";
         }
 
         return "------------------------------------------------------------------------------------------------------------------------------------\n"
